458d3a33598b87f8044bcdb200019aecda86a51b
Demostat101 <demostat1995@gmail.com>
2024-05-06 12:19:51 +0100

    add cors

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const bodyParser = require("body-parser");
 const db = require("./db");
 const routes = require("./routes");
 require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 
+
 app.use(bodyParser.json());
+app.use(cors({origin:"*", optionsSuccessStatus:200}));
 app.use("/", routes);
 app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+
 
 // route
 
